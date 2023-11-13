12221190929d51b1dd60fd802ff3ff16591d3c37
Nicoogf <nicolasgfalabella@gmail.com>
2023-11-13 10:58:21 -0300

    Grid de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { RiAddLine, RiCloseLine, RiMenu3Fill, RiPieChartLine, RiSearch2Line, RiUser3Line } from 'react-icons/ri';
+import { RiAddLine, RiArrowDownLine, RiCloseLine, RiMenu3Fill, RiPieChartLine, RiSearch2Line, RiUser3Line } from 'react-icons/ri';
 import './App.css' ;
 import Sidebar from './components/shared/Sidebar' ;
 import { useState } from 'react';
@@ -35,13 +35,13 @@ function App() {
       </button>
     </nav>
 
-    <main className='lg:pl-28 grid grid-cols-1 lg:grid-cols-8'>
+    <main className='lg:pl-32 grid grid-cols-1 lg:grid-cols-8 p-4 pb-20'>
 
-      <div className='lg:col-span-6 '> 
+      <div className='lg:col-span-6 md:p-8'> 
 
-      <header className='p-4'>
+      <header className=''>
 
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-col md:flex-row md:justify-between gap-4 mb-6 md-items-center'>
 
           <div>
             <h1 className='text-2xl text-gray-300'> Bistro Resto </h1>
@@ -58,7 +58,7 @@ function App() {
        
         </div>
         
-        <nav className='text-gray-300 flex items-center justify-between border-b'> 
+        <nav className='text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6'> 
 
           <a href='#' className='py-2 pr-4 border-[#ec7c6a] relative before:w-1/2 before:h-[2px] before:absolute before:bg-[#ec7c6a] before:left-0 before:rounded-full before:-bottom-[1px] text-[#ec7c6a]'> Hot dishes </a>
           <a href='#' className='py-2 pr-4'> Cold dishes </a>
@@ -68,6 +68,73 @@ function App() {
         </nav>
 
       </header>
+
+      <div className='flex items-center justify-between mb-16'>
+          <h2 className='text-xl text-gray-300'>Choose Dishes</h2>
+          <button className='flex items-center gap-4 text-gray-300 bg-[#1f1d2b] py-2 px-4 rounded-lg'> <RiArrowDownLine /> Dine in 
+          </button>
+      </div>
+
+      {/* Content */}
+      <div className='p-8 grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3'>
+        {/* Cart*/}
+        <div className='bg-[#1f1d2b] p-8 rounded-xl flex flex-col items-center text-gray-300 text-center gap-2'>
+
+          <img src="./plato.png" className='w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full'/>
+
+          <p className='text-xl'> Speacy seasone </p>
+          <span className='text-gray-400'> $2.29</span>
+          <p className='text-gray-600'> 20 Bowls Dispnibles</p>
+        </div>
+
+        <div className='bg-[#1f1d2b] p-8 rounded-xl flex flex-col items-center text-gray-300 text-center gap-2'>
+
+        <img src="./plato.png" className='w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full'/>
+
+        <p className='text-xl'> Speacy seasone </p>
+        <span className='text-gray-400'> $2.29</span>
+        <p className='text-gray-600'> 20 Bowls Dispnibles</p>
+        </div>
+
+        <div className='bg-[#1f1d2b] p-8 rounded-xl flex flex-col items-center text-gray-300 text-center gap-2'>
+
+        <img src="./plato.png" className='w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full'/>
+
+        <p className='text-xl'> Speacy seasone </p>
+        <span className='text-gray-400'> $2.29</span>
+        <p className='text-gray-600'> 20 Bowls Dispnibles</p>
+        </div>
+
+        <div className='bg-[#1f1d2b] p-8 rounded-xl flex flex-col items-center text-gray-300 text-center gap-2'>
+
+        <img src="./plato.png" className='w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full'/>
+
+        <p className='text-xl'> Speacy seasone </p>
+        <span className='text-gray-400'> $2.29</span>
+        <p className='text-gray-600'> 20 Bowls Dispnibles</p>
+        </div>
+
+        <div className='bg-[#1f1d2b] p-8 rounded-xl flex flex-col items-center text-gray-300 text-center gap-2'>
+
+          <img src="./plato.png" className='w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full'/>
+
+          <p className='text-xl'> Speacy seasone </p>
+          <span className='text-gray-400'> $2.29</span>
+          <p className='text-gray-600'> 20 Bowls Dispnibles</p>
+        </div>
+
+        <div className='bg-[#1f1d2b] p-8 rounded-xl flex flex-col items-center text-gray-300 text-center gap-2'>
+
+        <img src="./plato.png" className='w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full'/>
+
+        <p className='text-xl'> Speacy seasone </p>
+        <span className='text-gray-400'> $2.29</span>
+        <p className='text-gray-600'> 20 Bowls Dispnibles</p>
+        </div>
+
+      </div>
+
+
       
       </div>
 
