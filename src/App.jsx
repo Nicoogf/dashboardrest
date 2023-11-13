7025ee5197532afcc21123b8eee2b6b624da5f32
Nicoogf <nicolasgfalabella@gmail.com>
2023-11-12 22:56:00 -0300

    Fix right

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
     <main className='lg:pl-28 grid grid-cols-1 lg:grid-cols-8'>
       <div className='lg:col-span-6 '> Hola 1 </div>
-      <div className='lg:col-span-2 fixed lg:static right-0'> Carrito </div>
+      <div className='lg:col-span-2 fixed lg:static right-2 right-0'> Carrito </div>
     </main>
   </div>
   )
